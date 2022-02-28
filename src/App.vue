0b77a32de3fe579c312aba8ee1e113526c848af5
Nelson Hsu <nelsonhsu1027@gmail.com>
2022-02-28 18:47:16 +0800

    Finished Home layout

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar color="red lighten-3" dark fixed app>
+      <v-toolbar-title>青年班《耳根圓通》登錄簿</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <div class="d-none d-md-block">
+        <v-btn class="ma-2" outlined color="indigo"> 部數查詢 </v-btn>
+        <v-btn class="ma-2" outlined color="indigo"> 楞嚴咒 </v-btn>
+        <v-btn class="ma-2" outlined color="indigo"> 耳根圓通 </v-btn>
+      </div>
+
+      <div class="d-md-none">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-app-bar-nav-icon v-bind="attrs" v-on="on"></v-app-bar-nav-icon>
+        </template>
+        <v-list>
+           <v-btn class="ma-2" outlined color="indigo"> 部數查詢1 </v-btn>
+        <v-btn class="ma-2" outlined color="indigo"> 楞嚴咒 </v-btn>
+        <v-btn class="ma-2" outlined color="indigo"> 耳根圓通 </v-btn>
+        </v-list>
+      </v-menu>
+     </div>
+    </v-app-bar>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
+<script>
+export default {
+  name: "App",
+
+  data() {
+    return {
+      menu: [
+        { icon: "home", title: "Link A" },
+        { icon: "info", title: "Link B" },
+        { icon: "warning", title: "Link C" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+  /* font-size: 1.3em; */
+  font-family: Work Sans;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <iframe
+      src="https://docs.google.com/forms/u/2/d/e/1FAIpQLSdOfadDVUPocsZzQZMdgGdpPehpOTjV3cbvzOMpcoGaZt8-pA/viewform"
+      width="100%"
+      height="800px"
+      frameborder="1"
+    >
+    </iframe>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
+
+<style>
+</style>
